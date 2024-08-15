a698dd3a57e1ade05b6248ea94bacec9f704fccc
Ren <buher1994@gmail.com>
2024-08-15 22:49:31 +0200

    make build

---
+++ index.1ade6ac9.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll("thead, tbody, tfoot").forEach(function(e){e.querySelectorAll("tr").forEach(function(e){var t=e.querySelectorAll("th, td"),o=t[1].cloneNode(!0);e.insertBefore(o,t[t.length-1])})})});
+//# sourceMappingURL=index.1ade6ac9.js.map
